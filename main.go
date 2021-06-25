e8299c51c74304cf5d85f56d57ecac7faefc6b07
piann <shadow_hat@naver.com>
2021-06-25 16:03:11 +0900

    setup for REST

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/piann/coin_101/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/piann/coin_101/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
