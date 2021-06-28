06b337748d518108ed67d97ae4254563a28ef6ab
piann <shadow_hat@naver.com>
2021-06-28 15:21:56 +0900

    add API for adding block

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/piann/coin_101/blockchain"
+	"github.com/piann/coin_101/utils"
 )
 
 const port string = ":4000"
 
 type URL string
+
+type AddBlockBody struct {
+	Message string
+}
 
 func (u URL) MarshalText() ([]byte, error) {
 	url := fmt.Sprintf("http://localhost%s%s", port, u)
@@ -41,8 +48,25 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+
+	}
+
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
