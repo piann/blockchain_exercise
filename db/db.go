9b3980344cac15e69b48e5e570e0e44cc6a7ebd0
piann <shadow_hat@naver.com>
2021-07-10 20:29:43 +0900

    make db package

---
+++ db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/piann/coin_101/utils"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil { // init db
+		dbPtr, err := bolt.Open(dbName, 0600, nil)
+		db = dbPtr
+		utils.HandleErr(err)
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
+	}
+
+	return db
+}
